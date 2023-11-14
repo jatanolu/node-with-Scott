9564be9002118f3bed5a65bfe4e6e9b7ab8c101c
jatanolu <jacobrjean@gmail.com>
2023-11-14 14:47:01 +0100

    moving along

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import './src/command.js'
+// import data, {count} from './utils.js'
+// import fs from 'node:fs'
+// import _ from 'lodash'
+//
+// console.log("hello world");
+// const note = process.argv[2]
+//
+// const newNote = {
+//   content: note,
+//   id: Date.now()
+// }
+//
+// console.log(newNote)
---
+++ test.js
@@ -0,0 +1,15 @@
+import { writeFile } from 'node:fs'
+import fs from 'node:fs/promises'
+
+const readPjson = async () => {
+  const pjsonPath = new URL ('./package.json', import.meta.url).pathname
+  console.log(JSON.parse(await fs.readFile(pjsonPath, 'utf-8')))
+} 
+
+const writePjson = async () => {
+  const newFile = new URL ('./demo.js', import.meta.url).pathname
+  await fs.writeFile(newFile, 'console.log("yooooo!")')
+} 
+
+readPjson()
+writePjson()
